4f3964acefcd85e69b81271b8c873028bcb5cd31
Vorasiri <s6101012630143@email.kmutnb.ac.th>
2020-01-11 20:47:45 +0700

    Add User Story to functional_tests/tests.py

---
+++ functional_tests/tests.py
@@ -0,0 +1,24 @@
+# Tina has heard about a very amazing online lecture note app.
+# So she goes to check out its homepage.
+
+# She notices the page title ,upload button and also many lecture thumbnails.
+# She is invited to click on upload button.
+# It's bring her to upload lecture page.
+
+# There is button for uploading the pictures of lecture, she clicks and upload them.
+# There is the box for lecture name ,details and owner name​.
+# She fills the name and details into the box and set owner name as Tina.
+# The upload completed, the page refresh then she see her lecture on the homepage.
+​
+# She types “computer” into a text box (Tina's major is computer engineering).
+# When she hits the enter, the page refresh and the lectures about computer appear.
+
+# She chooses one of many lectures to find out.
+# She click on the thumbnail the page update and the lecture appear​.
+
+# She notices the arrow buttons and home button.
+# When she clicks on arrow button, then the next page of lecture appear​.
+
+# Satisfied, Tina goes back to sleep
+
+​
