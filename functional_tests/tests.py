95ac0338a7256e69c544fc1926377c40b1876d11
PIPE <s6101012610126@email.kmutnb.ac.th>
2020-01-14 15:35:44 +0700

    Update User Story

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(3)
         self.fail('Finish the test')
 
-"""
+
      def test_user_can_upload(self):
 # She notices the page title ,upload button and also many lecture thumbnails.
         upload_link = self.browser.find_element_by_link_text('')
@@ -31,8 +31,9 @@ class NewVisitorTest(unittest.TestCase):
         button = self.browser.find_element_by_id('')
         inputbox = button.find_elements_by_tag_name('input')
 
-# There is button for uploading the pictures of lecture, she clicks and upload them.
-# There is the box for lecture name ,details and owner name​.
+# There is button for uploading the pictures of lecture. 
+# She clicks and upload them.
+# There is the box for lecture name ,subject, description and owner name​.
         time.sleep(1)
         inputbox.send_pic
 
@@ -48,7 +49,7 @@ class NewVisitorTest(unittest.TestCase):
 
         time.sleep(1)
         inputbox.click() 
-# The upload completed, the page refresh then she see her lecture on the homepage.
+# She click on publish button, the page refresh then she see her lecture on the homepage.
         self.assertIn('lecture')
 
 
@@ -56,24 +57,26 @@ class NewVisitorTest(unittest.TestCase):
         button = self.browser.find_element_by_id('')
         inputbox = button.find_elements_by_tag_name('input')
         self.assertEqual(inputbox.get_attribute('value'), 'Search')
-# She types “computer” into a text box (Tina's major is computer engineering).
-# When she hits the enter, the page refresh and the lectures about computer appear.
+# She types “computer” into a text box (Tina's program is Sci-Math-Com).
+# When she hits the enter, the page refreshs and the lectures about computer appear.
         time.sleep(1)
         inputbox.send_keys('computer')
 
         time.sleep(1)
         inputbox.click() 
 # She chooses one of many lectures to find out.
-# She click on the thumbnail the page update and the lecture appear​.
+# She clicks on the thumbnail, the page update then the lecture appears​.
         self.assertIn('lecture')
-# She notices the arrow buttons and home button.
+# She notices the lecture name, owner name, description, arrow buttons and logo button.
 # When she clicks on arrow button, then the next page of lecture appear​.
         inputbox.send_keys(next)
-# Satisfied, Tina goes back to sleep
+# She reads all the page after that she clicks on homepage button.
+# Suddenly the page redirect to the homepage. 
+# Satisfied, Tina goes back to sleep.
         time.sleep(3)
         self.browser.quit()
 
-"""
+
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
 ​
