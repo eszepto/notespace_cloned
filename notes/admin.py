212c63068a5e942b829d855c4f71da518f600a3e
EsZe <33262870+eszepto@users.noreply.github.com>
2020-01-18 00:21:51 +0700

    add adminpage

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Note, Image, Tag
 # Register your models here.
+
+class ImageInline(admin.StackedInline):
+    model = Image
+    extra = 1
+
+class NoteAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
+    fieldsets = [
+        
+        ("", {'fields': ['index','name','desc','tags']})
+        
+    ]
+
+admin.site.register(Note, NoteAdmin)
--- notes/models.py
+++ notes/models.py
@@ -6,9 +6,24 @@ class Note(models.Model):
     index = models.IntegerField()
     name = models.TextField(max_length=200)
     desc = models.TextField(default='',max_length=1000)
+    tags = models.ManyToManyField('Tag')
+  
 
 class Image(models.Model):
     image = models.ImageField()
     note = models.ForeignKey(Note, on_delete=models.CASCADE)
 
+
+class Tag(models.Model):
+    title = models.CharField(max_length=250, blank=True)
+    slug = models.SlugField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = "tag"
+        verbose_name_plural = "tags"
+        ordering = ['title']
+    
+    
+    def get_absolute_url(self):
+     return "/tags/%s/" % self.slug
 
