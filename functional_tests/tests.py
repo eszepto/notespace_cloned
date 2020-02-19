43c9fbb9b3281e3f6a7eb69f7b9f62b24e751715
Vorasiri <s6101012630143@email.kmutnb.ac.th>
2020-02-19 23:17:27 +0700

    Add test note name, publish date, owner

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,19 +3,24 @@ from django.test import Client
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import datetime
 from selenium.common.exceptions import WebDriverException
 from notes.models import Note,Image
 class NewVisitorTest(LiveServerTestCase):
+    publishTime = ""
     def setUp(self):
         welcomeNote = Note()
         welcomeNote.name = "Welcome to Note Space!"
         welcomeNote.desc = "Introduction and Quick guide"
+        welcomeNote.owner = "Note Space Team"
         welcomeNote.save()
 
         testNote = Note()
         testNote.name = "Basic Economics"
         testNote.subject = "Economics"
-        testNote.desc = "This note is about demands, suplies and how market work."
+        testNote.desc = "This note is about demands, suplies and how market works."
+        testNote.owner = "Susan"
+        publishTime = datetime.datetime.now()
         testNote.save()
         # self.browser = webdriver.Edge()
         self.browser = webdriver.Firefox()
@@ -60,16 +65,14 @@ class NewVisitorTest(LiveServerTestCase):
         webpage = self.browser.find_element_by_tag_name("body")
         self.assertIn("searching for 'Economics'", webpage.text)
     # She notices the filter bar and filters the newest.
-    
     # She chooses the most relative note.
-        self.assertIn("Basic Economics", [item.text for item in search_results])
-
-    def test_user_can_view_the_note(self):
-        pass
     # She clicks on the thumbnail, the page update then the lecture appears​.
-    
+        self.assertIn("Basic Economics", [item.text for item in search_results])
     # She notices the lecture note name, owner name, and description.
-
+        note = self.browser.find_element_by_id('note')
+        self.assertIn('Basic Economics', [link.text for link in note.find_elements_by_tag_name('h1')])
+        self.assertIn(('Published %s',publishTime), [link.text for link in note.find_elements_by_tag_name('span')])
+        self.assertIn('By Susan', [link.text for link in note.find_elements_by_tag_name('span')])
     # She found left-right arrow buttons, note image between that and dots in the bottom.
 
     # When she clicks on the right arrow button, then the next page of the lecture appears​.
@@ -107,9 +110,9 @@ class NewVisitorTest(LiveServerTestCase):
         writerNameTextBox = self.browser.find_element_by_id('WriterName')
     # She clicks browse button and upload her note.
         self.browser.execute_script("arguments[0].style.display = 'block';", file_input_button)
-        file_input_button.send_keys("../static/TestNotes/Econ/IMG_0815.JPG")
-        file_input_button.send_keys("../static/TestNotes/Econ/IMG_0816.JPG")
-        file_input_button.send_keys("../static/TestNotes/Econ/IMG_0817.JPG")
+        file_input_button.send_keys("../static/TestNotes/Math/IMG_0815.JPG")
+        file_input_button.send_keys("../static/TestNotes/Math/IMG_0816.JPG")
+        file_input_button.send_keys("../static/TestNotes/Math/IMG_0817.JPG")
     # She fills the name and details into the box and sets the owner's name as Tina.
         lectureNameTextBox.send_keys('Elementary Logic')
         subjectTextBox.send_keys('Mathematics')
@@ -130,4 +133,3 @@ class NewVisitorTest(LiveServerTestCase):
     # and click on the arrow button to check that the order is correct.
 
     # She proud of herself and close the browser.
-        self.fail("test_user_can_upload OK!")
