fe1ed27fc7496f8fa4213461d0b28a4481e7ab02
Thanabordee <s5901012630075@email.kmutnb.ac.th>
2020-02-24 12:33:23 +0700

    Refactor some image slider FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -75,13 +75,13 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('By Susan', [link.text for link in note.find_elements_by_tag_name('span')])
 
 
-	left_arrow  = [link.text for link in note.find_elements_by_class_name('prev')]
-        right_arrow = [link.text for link in note.find_elements_by_class_name('next')]
+	left_arrow  = self.browser.find_elements_by_class_name('prev')
+        right_arrow = self.browser.find_elements_by_class_name('next')
     # She found left-right arrow buttons, note image between that and dots in the bottom.
         self.assertIn(('IMG_0809.JPG', [link.text for link in note.find_elements_by_class_name('mySlides')]
         self.assertIn('prev' , left_arrow)
         self.assertIn('next' , right_arrow)
-        self.assertIn('next' , [link.text for link in note.find_elements_by_class_name('dot')])
+        self.assertIn('dot' , self.browser.find_elements_by_class_name('dot')
     # When she clicks on the right arrow button, then the next page of the lecture appears​.
         time.sleep(1)
         right_arrow.click()
